81675156e7d5bd69e6647e77c163d635d652ff0e
ntn96 <39365787+ntn96@users.noreply.github.com>
2019-09-11 19:34:37 +0200

    Add files via upload

---
+++ TwoSumProblem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    class TwoSumProblem
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[]{ 1, 10, 25, 35, 60};
+            int[] result = TwoSum(nums, 60);
+            Console.WriteLine(nums[result[0]]+" + "+nums[result[1]]+" = 60");
+            Console.ReadKey(true);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int length = nums.Length;
+            Hashtable table = new Hashtable();
+            for (int i = 0; i < length; i++)
+            {
+                if (table.Contains(nums[i])) return new int[] { (int)table[nums[i]], i };
+                int complement = target - nums[i];
+                if (!table.Contains(complement))
+                    table.Add(complement, i);
+            }
+            return null;
+        }
+    }
+}
