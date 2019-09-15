08299cd199fb1610601d56a9a21bd5a5565f2261
ntn96 <39365787+ntn96@users.noreply.github.com>
2019-09-15 17:24:25 +0200

    Add files via upload

---
+++ LengthOfLongestSubstring.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    class LengthOfLongestSubstring
+    {
+        // You can find this problem in https://leetcode.com/problems/longest-substring-without-repeating-characters/
+        static void Main(string[] args)
+        {
+            string example1 = "abcabcbb";
+            string example2 = "bbbbb";
+            string example3 = "pwwkew";
+
+            Console.WriteLine("Input: " + example1);
+            Console.WriteLine("Output: " + LengthOfLongestSubstringSolution(example1));
+            Console.WriteLine("Input: " + example2);
+            Console.WriteLine("Output: " + LengthOfLongestSubstringSolution(example2));
+            Console.WriteLine("Input: " + example3);
+            Console.WriteLine("Output: " + LengthOfLongestSubstringSolution(example3));
+            Console.ReadKey(true);
+        }
+
+        public static int LengthOfLongestSubstringSolution(string s)
+        {
+            int max = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                HashSet<char> letras = new HashSet<char>();
+                letras.Add(s[i]);
+                int maxlocal = 1;
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (letras.Contains(s[j]))
+                    {
+                        if (maxlocal > max) max = maxlocal;
+                        break;
+                    }
+                    letras.Add(s[j]);
+                    maxlocal++;
+                }
+                if (maxlocal > max) max = maxlocal;
+            }
+            return max;
+        }
+    }
+}
