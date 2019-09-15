da4ddfdf77e6517ee664f67917e12bda89896cf5
ntn96 <39365787+ntn96@users.noreply.github.com>
2019-09-15 17:36:38 +0200

    Add files via upload

---
+++ ReverseInteger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    class ReverseInteger
+    {
+        //You can find this problem at https://leetcode.com/problems/reverse-integer/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input: 123");
+            Console.WriteLine("Output: " + Reverse(123));
+            Console.WriteLine("Input: -123");
+            Console.WriteLine("Output: " + Reverse(-123));
+            Console.WriteLine("Input: 120");
+            Console.WriteLine("Output: " + Reverse(120));
+            Console.ReadKey(true);
+        }
+
+        public static int Reverse(int x)
+        {
+            int result = 0, mod = 0;
+            while (x != 0)
+            {
+                mod = x % 10;
+                x /= 10;
+                result += mod;
+                try
+                {
+                    if (x != 0) result = checked(result * 10);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            return result;
+        }
+    }
+}
